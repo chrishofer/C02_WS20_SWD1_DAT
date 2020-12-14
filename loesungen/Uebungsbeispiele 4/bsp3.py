93e154f16b1893432c0f443bcbc9b426e5d7ca78
Christian Hofer <christian.hofer@campus02.at>
2020-12-14 21:16:12 +0100

    Added solutions Übungsbeispiele 4

--- loesungen/Uebungsbeispiele 4/bsp3.py
+++ loesungen/Uebungsbeispiele 4/bsp3.py
@@ -29,12 +29,51 @@ class Verwaltungsstrafe:
 
     @property
     def strafe(self):
-        # hier fehlt noch code
-        return self.__strafe
+        if self.anzahl == 1:
+            rabatt = 0.3
+        elif self.anzahl == 2:
+            rabatt = 0.2
+        elif self.anzahl == 3:
+            rabatt = 0.1
+        else:
+            rabatt = 0
+        return self.__strafe * (1 - rabatt)
 
     @property
     def anzahl(self):
         return self.__anzahl
+
+    def strafe(self, geschwindigkeitsueberschreitung: int):
+        self.__anzahl += 1
+        if geschwindigkeitsueberschreitung <= 20:
+            self.__strafe += 30
+        elif geschwindigkeitsueberschreitung <= 30:
+            self.__strafe += 50
+        elif geschwindigkeitsueberschreitung <= 50:
+            self.__strafe += 100
+        elif geschwindigkeitsueberschreitung <= 100:
+            self.__strafe += 500
+        else:
+            self.__strafe += 1500
+
+    def verbandspaket(self):
+        self.__anzahl += 1
+        self.__strafe += 25
+
+    def alkohol(self, promille: float):
+        if promille < 0.5:
+            return
+        self.__anzahl += 2
+        if promille <= 1.0:
+            self.__strafe += 100
+        elif promille <= 2.0:
+            self.__strafe += 400
+        elif promille <= 3.0:
+            self.__strafe += 1000
+        else:
+            self.__strafe += 5000
+
+
 
 if __name__ == '__main__':
     a = Verwaltungsstrafe("hansi", "hinterseer", "was299")
